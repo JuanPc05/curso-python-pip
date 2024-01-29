5329f9f77c2948cdec91abe30a435c18ea4e32cf
JuanPc05 <jpcastillor1@gmail.com>
2024-01-29 16:14:54 -0500

    hice cambios pero no se

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,23 @@
-import store
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3,4]
+
+@app.get('/contacts', response_class=HTMLResponse)
+def get_contacts():
+    return '''<h1>Hello, I'm a webpage</h1>
+            <p>I'm a paragraph</p>
+    '''
 
 def run():
-    store.get_categories()
+    uvicorn.run('main:app', reload=True)
 
 if __name__ == '__main__':
     run()
+
+    
